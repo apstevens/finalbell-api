ab5917a0afe47e9b388940280dcaa27f14748f2e
Andy Stevens <andypaulstevens@gmail.com>
2025-11-05 18:31:48 +0000

    Move TypeScript and type definitions to dependencies for Railway build

--- src/services/stripeService.ts
+++ src/services/stripeService.ts
@@ -6,7 +6,7 @@ if (!env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-    apiVersion: '2024-12-18.acacia',
+    apiVersion: '2025-10-29',
 });
 
 export interface CheckoutItem {
